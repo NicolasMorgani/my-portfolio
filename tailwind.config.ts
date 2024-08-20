05dfdc503646ff669a1f3ebcdb74c43d60ab682a
Nico Morgani <nico_morgani@hotmail.com>
2024-08-20 18:20:01 -0300

    background color

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -14,7 +14,7 @@ const config: Config = {
       },
       backgroundImage: {
         "gradient-cover":
-          "linear-gradient(90.21deg, rgba(3, 190, 221, 0.5) -5.91%, rgba(74, 47, 189, 0.5) 111.58%)",
+          "linear-gradient(90.21deg, rgba(0, 81, 242, 1) -5.91%, rgba(74, 47, 189, 0.5) 111.58%)",
       },
     },
   },
